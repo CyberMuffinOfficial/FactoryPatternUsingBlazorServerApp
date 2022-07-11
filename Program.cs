df2d97b90349be0534af21dbe10c99570a32e9f4
CyberMuffin <cybermuffinofficial@gmail.com>
2022-07-11 21:08:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using FactoryPatternUsingBlazorServerApp.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using FactoryPatternUsingBlazorServerApp.Samples;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+// FACTORY PATTERN STUFF HERE
+
+builder.Services.AddTransient<ISample1, Sample1>();
+
+//
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Samples/Sample1.cs
@@ -0,0 +1,11 @@
+﻿namespace FactoryPatternUsingBlazorServerApp.Samples;
+
+public interface ISample1
+{
+    string CurrentDateTime { get; set; }
+}
+
+public class Sample1 : ISample1
+{
+    public string CurrentDateTime { get; set; } = DateTime.Now.ToString();
+}
